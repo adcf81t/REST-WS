232286ffa86e27477d14f573cbd7a8505030333d
adcf81t <rabindra.saha@gmail.com>
2016-03-27 13:52:18 -0500

    changed sysout

--- src/com/jersey/rest/TestClient.java
+++ src/com/jersey/rest/TestClient.java
@@ -3,7 +3,7 @@ package com.jersey.rest;
 public class TestClient {
 
 	public static void main(String[] args) {
-		System.out.println("Test Client");
+		System.out.println("Test Client !");
 	}
 
 }
