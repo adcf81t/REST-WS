a429bd069daa080ee724de9ab202e909172961c6
adcf81t <rabindra.saha@gmail.com>
2016-04-06 01:33:14 -0500

    test1

--- src/main/java/com/jersey/rest/TestClient.java
+++ src/main/java/com/jersey/rest/TestClient.java
@@ -5,7 +5,7 @@ public class TestClient {
 
 	public static void main(String[] args) 
 	{
-		System.outprintln((System.getProperty("simple.message")==null?"":System.getProperty("simple.message")) + StringUtils.swapCase(args.length<=0?"":args[0]) + " from REST Web Service Test Client !!! ");
+		System.out.println((System.getProperty("simple.message")==null?"":System.getProperty("simple.message")) + StringUtils.swapCase(args.length<=0?"":args[0]) + " from REST Web Service Test Client !!! ");
 	}
 
 }
